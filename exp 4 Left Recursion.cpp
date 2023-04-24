ff62feb19aa8b2461f6f78aa3a5a0d90de7a0abb
thg999 <131686333+thg999@users.noreply.github.com>
2023-04-24 22:19:17 +0530

    Update exp 4 Left Recursion.cpp
    op

--- exp 4 Left Recursion.cpp
+++ exp 4 Left Recursion.cpp
@@ -71,3 +71,41 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+/*/tmp/yh6PGEI6ES.o
+Enter number of non terminals: 3
+Enter non terminals one by one: 
+Non terminal 1 : E
+Non terminal 2 : T
+Non terminal 3 : F
+Enter 'esp' for null
+Number of E productions: 2
+One by one enter all E productions
+RHS of production 1: E+T
+RHS of production 2: T
+Number of T productions: 2
+One by one enter all T productions
+RHS of production 1: T*F
+RHS of production 2: F
+Number of F productions: 2
+One by one enter all F productions
+RHS of production 1: (E)
+RHS of production 2: i
+110
+
+
+New set of non-terminals: E T F E' T' 
+
+New set of productions: 
+E -> TE'
+T -> FT'
+F -> (E)
+F -> i
+E' -> +TE'
+E' -> esp
+T' -> *FT'
+T' -> esp */
